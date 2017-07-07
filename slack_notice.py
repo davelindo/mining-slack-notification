62901729f2305381142d3c4221ab3c80fc58abae
Dave Lindon <github@davelindon.me>
2017-07-07 08:40:56 -0700

    slack notice

---
+++ slack_notice.py
@@ -0,0 +1,23 @@
+import urllib2
+import os
+
+slack_webhook = os.environ['SLACK_WEBHOOK']
+
+def slack_notification(slack_webhook, message):
+    slack_data = {'text': message}
+    request = urllib2.Request(slack_webhook)
+    request.add_header('Content-type', 'application/json')
+    try:
+        response = urllib2.urlopen(request, json.dumps(slack_data))
+    except:
+        print "Failed to connect to slack!"
+
+def build_message():
+    message = """Instance %s started Mining""" % (os.popen("curl -s http://169.254.169.254/latest/meta-data/instance-id").read())
+
+def main(event, context):
+    message = build_message()
+    slack_notification(slack_webhook,build_message())
+
+if __name__ == '__main__':
+    main('blah', 'blah')
